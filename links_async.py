fecd14f036f64594aac91e93ccf577469a959733
Aspect13 <ad13box@gmail.com>
2019-08-08 21:31:37 +0300

    added async version

---
+++ links_async.py
@@ -0,0 +1,95 @@
+import json
+import asyncio
+
+from sqlalchemy.exc import IntegrityError
+from ProductClass import Product, PAGE_PARAM
+from logger_custom import logger
+from models import ProductModel, ReviewModel, Session
+from utils_async import get_soup, get_pages_count, IsRedirectError
+
+
+async def download_item(session, product_soup, category_name):
+	db_product = ProductModel(category_description=category_name)
+	try:
+		product = Product.from_list_soup(list_soup=product_soup)
+		db_product.specs = str(product.specs)
+		db_product.yandex_id = product.id
+	except IsRedirectError:
+		logger.warning(f'Product is from other shop: {category_name}, {str(i)[:20]}')
+		product = Product.from_other_shop(list_soup=product_soup)
+		db_product.other_shop = True
+		db_product.other_shop_id = product.id
+	db_product.name = product.name
+	db_product.detail_url = product.detail_url
+	db_product.original_price = product.original_price
+	db_product.final_price = product.final_price
+
+	session.add(db_product)
+	for rv in product.reviews:
+		db_review = ReviewModel(**rv.model_data, product=db_product)
+		session.add(db_review)
+
+	try:
+		session.commit()
+	except IntegrityError as e:
+		logger.critical(f'Integrity error for category_name: {category_name} {product.detail_url}')
+		logger.critical(f'Integrity error for category_name: {product.name}#{product.id}')
+		session.rollback()
+		session.flush()
+
+
+# logger.critical('For test purposes continue...')
+# raise e
+
+
+async def download_link(soup, category_name=None):
+	session = Session()
+	product_list = soup.find('div', 'n-snippet-list').find_all('div', 'n-snippet-card2', recursive=False)
+
+	logger.debug('download_link got {} items'.format(len(product_list)))
+
+	tasks = []
+	for i in product_list:
+		tasks.append(asyncio.create_task(download_item(session=session, product_soup=i, category_name=category_name)))
+
+	print('LOOP END')
+	await asyncio.gather(*tasks)
+
+
+async def download_page(url_object):
+	category_name, url = url_object.get('category_name'), url_object['url']
+	# try: #todo: remove all tries like that
+	soup = await get_soup(url)
+	# except (ConnectionError, TimeoutError):
+	# 	logger.critical(f'Some shit happened to url: {url} :: 64')
+	# 	return
+	pages_count = get_pages_count(soup)
+
+	logger.debug(f'Found {pages_count} pages for list url {url}')
+	logger.debug('download_list::getting data from page 1')
+
+	tasks = [asyncio.create_task(download_link(soup, category_name))]
+
+	for page_num in range(2, pages_count + 1):
+		logger.debug(f'download_list::getting data from page {page_num}')
+		new_url = '{}&{}'.format(url, PAGE_PARAM.format(page_num))
+		soup = await get_soup(new_url)
+		tasks.append(asyncio.create_task(download_link(soup, category_name)))
+
+	await asyncio.gather(*tasks)
+
+
+async def main(link_set):
+	while link_set:
+		url_object_form_json = link_set.pop()
+		logger.debug(f'url_object_form_json {str(url_object_form_json)}')
+		tasks.append(asyncio.create_task(download_page(url_object_form_json)))
+	print(tasks)
+	await asyncio.gather(*tasks, return_exceptions=True)
+
+
+if __name__ == '__main__':
+	ls = json.load(open('link_set.json', 'r', encoding='utf8'))
+	logger.info('START\n')
+	tasks = []
+	asyncio.run(main(ls), debug=True)
---
+++ utils_async.py
@@ -0,0 +1,98 @@
+import asyncio
+from pathlib import Path
+
+import aiohttp
+import requests
+import json
+from bs4 import BeautifulSoup
+from uuid import uuid4
+from time import sleep
+
+from logger_custom import logger
+
+CACHED_FOLDER = 'cached'
+
+
+class IsRedirectError(Exception):
+	# def __init__(self, *args, **kwargs):
+	# 	print('IsRedirectError', args, kwargs)
+	# 	pass
+	msg = 'Link is a redirect'
+
+
+async def fetch(session, url):
+	async with session.get(url) as response:
+		return await response.text()
+
+
+async def get_data(url):
+	if is_redirect(url):
+		logger.warning(f'URL is a redirect: {url}')
+		raise IsRedirectError
+	cache_dict_file = 'cache_dict.json'
+	cache_dict = json.load(open(cache_dict_file, 'r'))
+	file_name = cache_dict.get(url, None)
+	if not file_name:
+		file_name = '{}.html'.format(str(uuid4())[:8])
+		contents = await cache_html(url, file_name)
+		cache_dict[url] = file_name
+		json.dump(cache_dict, open(cache_dict_file, 'w'))
+	else:
+		try:
+			contents = open(f'{CACHED_FOLDER}/{file_name}', 'r').read()
+			logger.info(f'Using cached: {file_name} for url: {url}')
+		except FileNotFoundError:
+			del cache_dict[url]
+			json.dump(cache_dict, open(cache_dict_file, 'w'))
+			return get_data(url)
+	return contents
+
+
+async def cache_html(url, name, attempts=1):
+	MAX_GET_ATTEMPTS = 3
+	TIMEOUT_SEC = 10
+
+	if attempts > MAX_GET_ATTEMPTS:
+		logger.critical(f'Tried {MAX_GET_ATTEMPTS} times to get URL {url}')
+		raise TimeoutError(f'Tried {MAX_GET_ATTEMPTS} times to get URL {url}')
+	logger.info(f'GET: {url}')
+	if attempts > 1:
+		logger.info(f'attempt: {attempts}')
+	async with aiohttp.ClientSession() as session:
+		site = await fetch(session, url)
+		# print('SSSSSSSSSSSs', site)
+	if is_captcha(site):
+		logger.warning(f'Captcha received for url: {url}')
+		logger.warning(f'sleeping for {TIMEOUT_SEC * attempts}s...')
+		await asyncio.sleep(TIMEOUT_SEC * attempts)
+		return await cache_html(url, name, attempts=attempts+1)
+	with open(f'{CACHED_FOLDER}/{name}', 'wb') as out:
+		out.write(site)
+	logger.info(f'Cache name: {name}')
+	return site
+
+
+def is_captcha(html):
+	return b'<a class="link" href="//yandex.ru/support/captcha/"' in html
+
+
+def is_redirect(url):
+	return '/redir/' in url
+
+
+async def get_soup(url):
+	site_data = await get_data(url)
+	# site_data = get_data(url)
+	return BeautifulSoup(site_data, 'html.parser')
+
+
+def get_pages_count(soup):
+	pagebar = soup.find('div', 'n-pager')
+	if pagebar:
+		return int(json.loads(pagebar['data-bem'])['n-pager']['pagesCount'])
+	return 1
+
+
+def write_tmp_soup(soup):
+	with open(f'{Path(__file__).absolute().parent}/sitecache.html', 'w') as out:
+		out.write(soup.prettify())
