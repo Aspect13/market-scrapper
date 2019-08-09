47ca4e0bc527b4ec34e302c13d230732cbbc253b
Aspect13 <ad13box@gmail.com>
2019-08-09 17:47:43 +0300

    smallest fix

--- asyncronous/utils.py
+++ asyncronous/utils.py
@@ -17,7 +17,7 @@ class IsRedirectError(Exception):
 
 
 async def fetch(session, url):
-	async with session.get(url, headers=HEADERS) as response:
+	async with session.get(url, headers=HEADERS()) as response:
 		return await response.text()
 
 
